64df47c255ca043df61212f3a4260c0b9c5e8ecb
MichaelKuzm <49190493+MichaelKuzm@users.noreply.github.com>
2019-04-03 22:25:51 +0300

    Add files via upload

---
+++ src/main/java/com/example/javaPrime/JavaPrimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaPrime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPrimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPrimeApplication.class, args);
+	}
+
+}
